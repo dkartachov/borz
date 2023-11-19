6e5243f825b0dd6ce715427c8950609cd70cb171
Denis Kartachov <kartachovd@gmail.com>
2023-11-19 15:48:50 -0500

    remove unused code

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,59 +1,27 @@
 package manager
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
 	"log"
-	"net/http"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/dkartachov/borz/internal/manager/api"
 	"github.com/dkartachov/borz/internal/manager/database"
 	"github.com/dkartachov/borz/internal/manager/scheduler"
 	"github.com/dkartachov/borz/internal/model"
-	"github.com/dkartachov/borz/internal/task"
 	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
 )
-
-type Signal struct {
-	ShutdownAPI           chan struct{}
-	ShutdownTaskScheduler chan struct{}
-}
-
-type Manager struct {
-	Name          string
-	Workers       []string // worker addresses
-	nextWorker    int
-	Queue         chan task.Task
-	tasks         map[uuid.UUID]task.Task
-	taskWorkerMap map[uuid.UUID]string
-	Signal        Signal
-}
 
 func Run(args []string) {
 	name := args[0]
 	port, _ := strconv.Atoi(args[1])
 	workers := strings.Split(args[2], ",")
-	m := Manager{
-		Name:          name,
-		Workers:       workers, // TODO need a better way to instantiate this array
-		nextWorker:    0,
-		Queue:         make(chan task.Task),
-		tasks:         make(map[uuid.UUID]task.Task),
-		taskWorkerMap: make(map[uuid.UUID]string),
-		Signal:        Signal{ShutdownAPI: make(chan struct{}), ShutdownTaskScheduler: make(chan struct{})},
-	}
 
 	db := database.Database{
 		Workers:     workers,
 		Deployments: make(map[string]model.Deployment),
 		Pods:        make(map[string]model.Pod),
 	}
-
 	sched := scheduler.Scheduler{
 		PodQueue:        queue.New(),
 		PodNameByWorker: make(map[string]string),
@@ -61,8 +29,7 @@ func Run(args []string) {
 		NextWorker:      0,
 		Database:        &db,
 	}
-
-	a := api.Server{
+	server := api.Server{
 		Manager:   name,
 		Address:   "localhost",
 		Port:      port,
@@ -70,117 +37,8 @@ func Run(args []string) {
 		Database:  &db,
 	}
 
-	// go m.runTaskScheduler()
-	// go m.updateTasks(1000)
 	go sched.Start()
-	a.Start()
+	server.Start()
 
-	log.Printf("[%s] exiting", m.Name)
-}
-
-func (m *Manager) AddTask(t task.Task) {
-	m.Queue <- t
-}
-
-func (m *Manager) GetTasks() []TaskResponse {
-	var taskResp []TaskResponse = []TaskResponse{}
-
-	for _, t := range m.tasks {
-		taskResp = append(taskResp, TaskResponse{Task: t, Worker: m.taskWorkerMap[t.Id]})
-	}
-
-	return taskResp
-}
-
-// TODO Improve how workers are selected to send tasks to. This is a first pass round-robin approach
-func (m *Manager) selectWorker(taskId uuid.UUID) string {
-	if w, ok := m.taskWorkerMap[taskId]; ok {
-		return w
-	}
-
-	if m.nextWorker == len(m.Workers)-1 {
-		m.nextWorker = 0
-	} else {
-		m.nextWorker += 1
-	}
-
-	return m.Workers[m.nextWorker]
-}
-
-func (m *Manager) sendTask(t task.Task) {
-	log.Printf("[%v] sending task %s", m.Name, t.Name)
-	// CHECKME Is there a better place to set this?
-	if t.State == task.Pending {
-		t.State = task.Scheduled
-	}
-
-	w := m.selectWorker(t.Id)
-	log.Printf("[%v] selected worker %v", m.Name, w)
-
-	req, err := json.Marshal(t)
-	if err != nil {
-		log.Printf("[%v] error marshaling task request %v", m.Name, t.Id)
-		return
-	}
-
-	resp, err := http.Post(fmt.Sprintf("%s/tasks", w), "application/json", bytes.NewBuffer(req))
-	if err != nil {
-		log.Printf("[%v] error connecting to %s", m.Name, w)
-		return
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		log.Printf("[%v] error sending task to %s", m.Name, w)
-		resp.Body.Close()
-		return
-	}
-
-	resp.Body.Close()
-	m.taskWorkerMap[t.Id] = w
-}
-
-func (m *Manager) updateTasks(intervalMillis int) {
-	for {
-		select {
-		case <-m.Signal.ShutdownTaskScheduler:
-			log.Printf("[%s] shutting down task updater", m.Name)
-			return
-		default:
-			log.Printf("[%v] updating tasks", m.Name)
-			for _, w := range m.Workers {
-				resp, err := http.Get(fmt.Sprintf("%s/tasks", w))
-				if err != nil {
-					log.Printf("[%v] error connecting to %s", m.Name, w)
-					continue
-				}
-
-				if resp.StatusCode != http.StatusOK {
-					log.Printf("[%v] error getting tasks from %s", m.Name, w)
-					resp.Body.Close()
-					continue
-				}
-
-				var tasks []task.Task
-				json.NewDecoder(resp.Body).Decode(&tasks)
-
-				for _, t := range tasks {
-					m.tasks[t.Id] = t
-					m.taskWorkerMap[t.Id] = w
-				}
-			}
-			time.Sleep(time.Millisecond * time.Duration(intervalMillis))
-		}
-	}
-}
-
-func (m *Manager) runTaskScheduler() {
-	for {
-		select {
-		case <-m.Signal.ShutdownTaskScheduler:
-			log.Printf("[%s] shutting down task scheduler", m.Name)
-			return
-		case t := <-m.Queue:
-			go m.sendTask(t)
-		}
-	}
+	log.Printf("[%s] exiting", name)
 }
--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -1,42 +1,19 @@
 package worker
 
 import (
-	"fmt"
 	"log"
 	"strconv"
-	"sync"
-	"time"
 
-	"github.com/dkartachov/borz/internal/docker"
 	"github.com/dkartachov/borz/internal/model"
-	"github.com/dkartachov/borz/internal/task"
 	"github.com/dkartachov/borz/internal/worker/api"
 	"github.com/dkartachov/borz/internal/worker/borzlet"
 	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
 )
-
-type Worker struct {
-	name   string
-	Queue  chan task.Task
-	tasks  map[uuid.UUID]task.Task
-	Signal Signal
-}
-
-type Signal struct {
-	ShutdownAPI        chan struct{}
-	ShutdownTaskRunner chan struct{}
-}
 
 func Run(args []string) {
 	name := args[0]
 	port, _ := strconv.Atoi(args[1])
-	w := Worker{
-		name:   name,
-		Queue:  make(chan task.Task),
-		tasks:  make(map[uuid.UUID]task.Task), // CHECKME connect to database instead of storing tasks in memory?
-		Signal: Signal{ShutdownAPI: make(chan struct{}), ShutdownTaskRunner: make(chan struct{})},
-	}
+
 	s := borzlet.Store{
 		Pods: make(map[string]model.Pod),
 	}
@@ -44,132 +21,14 @@ func Run(args []string) {
 		JobQueue: queue.New(),
 		Store:    &s,
 	}
-	a := api.API{
+	server := api.API{
 		Address: "localhost",
 		Port:    port,
-		Worker:  w.name,
+		Worker:  name,
 		Borzlet: &b,
 	}
 	go b.Start()
-	// go w.runTasks()
-	a.Start()
+	server.Start()
 
-	log.Printf("[%v] exiting", w.name)
-}
-
-func (w *Worker) Name() string {
-	return w.name
-}
-
-func (w *Worker) AddTask(t task.Task) {
-	w.Queue <- t
-}
-
-func (w *Worker) StartTask(t task.Task) error {
-	log.Printf("[%v] starting task %v", w.name, t.Id)
-
-	d := docker.Docker{Image: t.Image}
-	containerID, err := d.Start()
-	if err != nil {
-		log.Printf("[%s] error starting task %v: %v", w.name, t.Id, err)
-	}
-
-	t.ContainerID = containerID
-	t.State = task.Running
-	t.StartTime = time.Now().UTC()
-	w.tasks[t.Id] = t
-
-	return nil
-}
-
-func (w *Worker) StopTask(t task.Task) error {
-	log.Printf("[%v] stopping task %v", w.name, t.Id)
-
-	_, ok := w.tasks[t.Id]
-	if !ok {
-		return fmt.Errorf("[%v] task %v not found", w.name, t.Id)
-	}
-
-	d := docker.Docker{ContainerID: t.ContainerID}
-	if err := d.Stop(); err != nil {
-		return err
-	}
-
-	t.State = task.Completed
-	t.FinishTime = time.Now().UTC()
-	w.tasks[t.Id] = t
-
-	return nil
-}
-
-func (w *Worker) Task(id uuid.UUID) (task.Task, bool) {
-	t, exists := w.tasks[id]
-	return t, exists
-}
-
-func (w *Worker) GetTasks() []task.Task {
-	var tasks []task.Task = []task.Task{}
-
-	for _, t := range w.tasks {
-		tasks = append(tasks, t)
-	}
-
-	return tasks
-}
-
-// CHECKME Dequeue ALL tasks and run them concurrently using goroutines? NO
-func (w *Worker) runTasks() {
-	for {
-		select {
-		case <-w.Signal.ShutdownTaskRunner:
-			log.Printf("[%v] shutting down task runner", w.name)
-			log.Printf("[%v] stopping tasks", w.name)
-			var wg sync.WaitGroup
-			for _, t := range w.tasks {
-				wg.Add(1)
-				go func(t task.Task) {
-					w.StopTask(t)
-					wg.Done()
-				}(t)
-			}
-			wg.Wait()
-			// CHECKME anything else?
-			close(w.Signal.ShutdownAPI)
-			return
-		case t := <-w.Queue:
-			go func() {
-				log.Printf("[%v] running task %s", w.name, t.Name)
-				if err := w.runTask(t); err != nil {
-					log.Printf("[%v] error running task %s: %v", w.name, t.Name, err)
-				}
-			}()
-		}
-	}
-}
-
-func (w *Worker) runTask(taskQueued task.Task) error {
-	// ti := w.TaskQueue.Dequeue()
-	// taskQueued := ti.(task.Task)
-	taskPersisted, ok := w.tasks[taskQueued.Id]
-	if !ok {
-		taskPersisted = taskQueued
-		newTask := taskQueued
-		w.tasks[taskQueued.Id] = newTask
-	}
-
-	var err error = nil
-	if task.ValidStateTransition(taskPersisted.State, taskQueued.State) {
-		switch taskQueued.State {
-		case task.Scheduled:
-			err = w.StartTask(taskQueued)
-		case task.Stopping:
-			err = w.StopTask(taskPersisted)
-		default:
-			err = fmt.Errorf("state transition to %v not supported", taskQueued.State)
-		}
-	} else {
-		err = fmt.Errorf("invalid state transition %v to %v", taskPersisted.State, taskQueued.State)
-	}
-
-	return err
+	log.Printf("[%v] exiting", name)
 }
